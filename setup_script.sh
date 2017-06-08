ff278d25cea01d7dfc0efcfd0e69fce7f8171fba
joepol <cpumaster15@gmail.com>
2017-06-08 10:25:23 +0300

    Add files via upload

---
+++ setup_script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python-pip python-dev
+sudo apt install python3-pip
+sudo pip3 install tensorflow
+pip3 install wheel
+pip3 install pandas
+
+sudo apt-get install python3-matplotlib
+sudo apt-get install python3-tk
+sudo apt-get install python3-sklearn
+
+pip3 install --upgrade pip
+sudo pip install -U setuptools
+sudo pip3 install jupyter
+pip install image
+sudo pip3 install prettytensor
+
+sudo apt-get install git
+
+sudo apt-get install libc6-dev 
+
+sudo apt-get install python-numpy  
+
+sudo pip3 install opencv-python
+#sudo pip install opencv-python
+
+sudo apt install ncftp
+#install softwares
+sudo apt-get install gparted
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get install sublime-text-installer
+
+# set sublime as your default text editor
+
+# optional - depends on project
+# sudo git clone https://github.com/tensorflow/tensorflow
